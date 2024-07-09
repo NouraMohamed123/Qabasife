92d6fbb6681558976477accf74d18e8f41fbcbf5
NouraMohamed123 <nouramohamed091@gmail.com>
2024-07-09 18:23:22 +0300

    شيي

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,25 +12,30 @@ class UserResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-
+    public function with($request)
+    {
+        return [
+            'status' => 'true',
+            'message' => 'تمت العميله بنجاح'
+        ];
+    }
 
 
     public function toArray($request)
     {
 
+
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'number' => $this->phone,
-             'photo' => asset('uploads/personal_photo/' . $this->photo),
-            'date_of_birth' => $this->date_of_birth,
-            'national_id' => $this->national_id,
+            'name' => $this->name??null,
+            'email' => $this->email??null,
             'phone' => $this->phone,
+            'date_of_birth' => $this->date_of_birth??null,
+            'national_id' => $this->national_id??null,
+            'photo' => asset('uploads/personal_photo/' . $this->photo),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'roles_name' => $this->roles->pluck('name')
-        ];
+            'roles_name' => $this->roles->first()?->name ?? null,      ];
     }
 }
 
